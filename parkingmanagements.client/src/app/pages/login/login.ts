9df5e362d40ade0a1bcdb1fb17ce9d6f22b06e4b
reiAndroid <krajae265@gmail.com>
2025-11-19 08:51:31 +0100

    idk

--- parkingmanagements.client/src/app/pages/login/login.ts
+++ parkingmanagements.client/src/app/pages/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
 export class Login {
+  //get the user data
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogIn() {
+    if (this.email && this.password) {
+      console.log("Login successful!");
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Please enter username and password'
+    }
+  }
 }
