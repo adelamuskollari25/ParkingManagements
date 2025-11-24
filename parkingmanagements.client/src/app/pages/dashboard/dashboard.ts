731a4cae7baeb5b6c9d4c3a05088befd621d7d58
reiAndroid <krajae265@gmail.com>
2025-11-24 17:38:57 +0100

    added style to the dashboard

--- parkingmanagements.client/src/app/pages/dashboard/dashboard.ts
+++ parkingmanagements.client/src/app/pages/dashboard/dashboard.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.scss'],
 })
 export class Dashboard {
   //get date
   today: Date = new Date();
+  greeting: string = '';
+  icon: string = '';
+
+  ngOnInit() {
+    this.setGreeting();
+  }
+
+  // set the icon and greeting depending on time
+  setGreeting() {
+    const hour = this.today.getHours();
+
+    if (hour < 5) {
+      this.icon = 'star';
+      this.greeting = 'Heyyy';
+    } else if (hour < 12) {
+      this.icon = 'wb_sunny';
+      this.greeting = 'Good Morning!';
+    } else if (hour < 17) {
+      this.icon = 'light_mode';
+      this.greeting = 'Good Afternoon!';
+    } else if (hour < 21) {
+      this.icon = 'nightlight';
+      this.greeting = 'Good Evening!';
+    } else {
+      this.icon = 'star';
+      this.greeting = 'Heyyy';
+    }
+  }
 }
