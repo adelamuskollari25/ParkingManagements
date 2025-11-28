d18fa65cb6c6faf5d64ed0ae9f6105d1034ada92
reiAndroid <krajae265@gmail.com>
2025-11-28 23:13:13 +0100

    created user profile and stylized it

--- parkingmanagements.client/src/app/pages/users/users-list/users-list.ts
+++ parkingmanagements.client/src/app/pages/users/users-list/users-list.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './users-list.html',
   styleUrl: './users-list.scss',
 })
 export class UsersList {
+  pwdHide = true;
 
 }
