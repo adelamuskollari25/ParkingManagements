6a0a34cd00039155a7e496cf583dd98b26988a4f
reiAndroid <krajae265@gmail.com>
2025-11-29 23:42:52 +0100

    added the entry ticket component and style

--- parkingmanagements.client/src/app/app.routes.ts
+++ parkingmanagements.client/src/app/app.routes.ts
@@ -7,12 +7,16 @@ import { FinancialReports } from './pages/financial-reports/financial-reports';
 import { Spots } from './pages/spots/spots';
 import { LotsList } from './pages/lots/lots-list/lots-list';
 import { UsersList } from './pages/users/users-list/users-list';
+import { TicketEntry } from './pages/tickets/ticket-entry/ticket-entry';
+import { TicketExit } from './pages/tickets/ticket-exit/ticket-exit';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: Login, title: 'Log In'},
     {path: 'dashboard', component: Dashboard, title: 'Dashboard'},
     {path: 'dashboard/ticket', component: TicketsList, title: 'Tickets'},
+    {path: 'dashboard/ticket/entry-ticket', component: TicketEntry, title: 'Entry Ticket'},
+    {path: 'dashboard/ticket/exit-ticket', component: TicketExit, title: 'Exit Ticket'},
     {path: 'dashboard/tariff', component: TariffView, title: 'Tariffs'},
     {path: 'dashboard/financial-report', component: FinancialReports, title: 'Financial Report'},
     {path: 'dashboard/spots', component: Spots, title: 'Parking Spots'},
