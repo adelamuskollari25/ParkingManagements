3d9b00ab79618fbc753a2b3b858f65f548966435
reiAndroid <krajae265@gmail.com>
2025-12-10 21:48:19 +0100

    some changes in TicketDTO

--- ParkingManagements.Server/DTOs/TicketDTOs/TicketDTO.cs
+++ ParkingManagements.Server/DTOs/TicketDTOs/TicketDTO.cs
@@ -10,6 +10,7 @@ namespace ParkingManagements.Server.DTOs.Ticket
         public DateTime EntryTime { get; set; }
         public DateTime? ExitTime { get; set; }
         public TicketStatus Status { get; set; }
+        public string SpotCode { get; set; } // NEW
         public decimal? ComputedAmount { get; set; }
         public bool Paid { get; set; }
         public VehicleDTO Vehicle { get; set; }
@@ -19,6 +20,8 @@ namespace ParkingManagements.Server.DTOs.Ticket
     {
         public Guid Id { get; set; }
         public string Plate { get; set; }
+        public VehicleType? Type { get; set; }  // NEW
+        public string? Color { get; set; }      // NEW
     }
 
 }
